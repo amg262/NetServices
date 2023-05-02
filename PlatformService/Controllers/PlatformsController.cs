3fd03e0ec1db42927a0938f11ab59cd42cc2af2b
Andrew Gunn <agunn@ellsworth.com>
2023-05-02 12:54:37 -0500

    Fixed the errors I was getting by replacing the controller which was somehow gone

---
+++ PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+using PlatformService.Models;
+
+namespace PlatformService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        // This is a dependency injection
+        private readonly IPlatformRepo _repository;
+        private readonly IMapper _mapper;
+
+        public PlatformsController(IPlatformRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("--> Getting Platforms....");
+
+            var platformItem = _repository.GetAllPlatforms();
+
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItem)); // 200 OK will return all platforms
+        }
+
+        // GET api/platforms/{id}
+        [HttpGet("{id:int}", Name = "GetPlatformById")]
+        public ActionResult<PlatformReadDto> GetPlatformById(int id)
+        {
+            var platformItem = _repository.GetPlatformById(id);
+            if (platformItem != null)
+            {
+                return Ok(_mapper.Map<PlatformReadDto>(platformItem)); // 200 OK  will return a platform by its id
+            }
+
+            return NotFound(); // This will return a 404 error if the platform is not found
+        }
+    }
+}
