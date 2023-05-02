65283386972891305502e8655be6e804b77f0665
Andrew Gunn <agunn@ellsworth.com>
2023-05-02 12:11:30 -0500

    Update PlatformsProfile.cs
    Profile mappings good for now

--- PlatformService/Profiles/PlatformsProfile.cs
+++ PlatformService/Profiles/PlatformsProfile.cs
@@ -1,9 +1,17 @@
 ﻿using AutoMapper;
+using PlatformService.Dtos;
+using PlatformService.Models;
 
 namespace PlatformService.Profiles
 {
     public class PlatformsProfile : Profile
     {
-
+        // This is where we will define our mappings
+        public PlatformsProfile()
+        {
+            // Source (Model / Platform model) -> Target (DTO / PlatformReadDto)
+            CreateMap<Platform, PlatformReadDto>(); // This is a mapping from Platform Model to PlatformReadDto
+            CreateMap<PlatformCreateDto, Platform>(); // This is a mapping from PlatformCreateDto to Platform
+        }
     }
 }
